7bf7c8c0c95c99aa67e7ea2a8e43b68d42d1222e
Gabriela <gasantos@ucsd.edu>
2026-04-28 11:37:25 -0700

    Updates

---
+++ _next/static/dRYeNsBtazczl9CEH8aO-/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/COGS-125-Project//_next/:path+",
+        "destination": "/COGS-125-Project/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
