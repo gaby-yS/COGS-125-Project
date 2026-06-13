959f0d2305b07468e2b03f08b36482703dd580fa
Gabriela <gasantos@ucsd.edu>
2026-06-13 12:27:10 -0700

    fix projects error

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
 import { useState } from "react";
-import Navbar from "@/app/components/Navbar";";
+import Navbar from "@/app/components/Navbar";
 
 export default function projects() {
   const [show, setShow] = useState(false);
