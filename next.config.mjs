06e4a9baf5e5225e2244a0d370387e1fea93adda
Gabriela <gasantos@ucsd.edu>
2026-06-13 18:59:46 -0700

    possible fix of deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === 'production';
-
-const nextConfig = {
-  reactCompiler: true,
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: isProd ? '/COGS-125-Project/' : '',
-  basePath: isProd ? '/COGS-125-Project' : '',
-  output: 'export',
-};
+const nextConfig = {};
 
 export default nextConfig;
